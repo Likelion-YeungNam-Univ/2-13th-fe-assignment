896aa978387722426f308eef6e2bdc01bedf9bc5
Slity <0911kjw@naver.com>
2025-04-09 01:20:43 +0900

    mobile-login 이벤트 추가
    1. 767px 이하의 미디어 쿼리에서 mobile-login 버튼을 누를 시 로그인 폼이 뜨도록 이벤트 추가
    
    2, txt-box와 list에 마우스 올리면 색상이 더 자연스럽게 변하도록 transition 추가

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.querySelector(".list_bar");
+  const menuButton = document.querySelector(".list_button");
   const list = document.querySelector(".list");
+  const mobileLoginButton = document.querySelector(".mobile-login");
+  const loginForm = document.querySelector(".login-form");
 
   menuButton.addEventListener("click", () => {
-      list.classList.toggle("active");
+    list.classList.toggle("active");
+  });
+  
+  mobileLoginButton.addEventListener("click", () => {
+    loginForm.classList.toggle("show");
+    mobileLoginButton.style.display = "none";
   });
 });
